26b70c50aec9e02eb92b805951f6583f1532d6a8
Santiago García <santyelprog@gmail.com>
2026-09-01 08:50:20 -0500

    Update recognition model

--- reconocimiento_digitos.m
+++ reconocimiento_digitos.m
@@ -4,7 +4,7 @@ salidas = readmatrix('salidas.csv');
 P = entradas;      
 T = salidas;
 
-net = newff([min(P')' max(P')'],[25, 20, 15, 10, 10],{'tansig', 'logsig', 'satlin', 'tansig', 'purelin'},'traingdm');
+net = newff([min(P')' max(P')'],[30, 25, 20, 15, 10],{'tansig', 'logsig', 'satlin', 'tansig', 'purelin'},'traingdm');
 
 net.trainParam.epochs = 10000;  
 net.trainParam.goal = 0.5e-3;   
@@ -14,11 +14,3 @@ net.trainParam.mc = 0.4;
 net = train(net, P, T); 
 
 C = sim(net, P); 
-
-[~, clasePredicha] = max(C);     
-[~, claseReal] = max(T);        
-aciertos = sum(clasePredicha == claseReal);
-total = length(claseReal);
-precision = aciertos / total * 100;
-
-fprintf('Precisión: %.2f%%\n', precision);
